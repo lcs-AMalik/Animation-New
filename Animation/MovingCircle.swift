b0aa2502f403dbc2f1669b1d712bc9e52026e8e2
Abdul <AMalik@lcs.on.ca>
2020-11-06 10:03:39 -0500

    Defining new circles

---
+++ Animation/MovingCircle.swift
@@ -0,0 +1,26 @@
+//
+//  Moving circle.swift
+//  Animation
+//
+//  Created by Abdul Malik on 2020-11-06.
+//
+
+import Foundation
+
+// A class is just a container that stores:
+// - state(information / data)
+// - behaviors ( methods / functions that make something happen)
+class MovingCircle {
+    
+    // 1. properties (the state)
+    var x: Int
+    var y: Int
+    var dx = Int
+    var dy = Int
+    var size: Int
+    
+    // 2. Intializer (initialize, or "set up" the properties with a first value)
+    
+    // 3.  Methods (make things happen)
+}
+
