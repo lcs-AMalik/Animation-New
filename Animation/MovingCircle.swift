819ea6e7432d23af7a9b41503c5f4888c0205b55
Abdul <AMalik@lcs.on.ca>
2020-11-07 10:07:02 -0500

    I have added the bounce

--- Animation/MovingCircle.swift
+++ Animation/MovingCircle.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CanvasGraphics
 
 // A class is just a container that stores:
 // - state(information / data)
@@ -15,12 +16,64 @@ class MovingCircle {
     // 1. properties (the state)
     var x: Int
     var y: Int
-    var dx = Int
-    var dy = Int
-    var size: Int
+    var dx: Int
+    var dy: Int
+    var diameter: Int
     
     // 2. Intializer (initialize, or "set up" the properties with a first value)
     
+    init(x: Int, y: Int, dx: Int, dy: Int, diameter: Int) {
+        
+        // "self" refers to the properties from This class
+        // light blue refers to property
+        // the black refers to the local parameter value
+        
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.diameter = diameter
+        
+    }
+    
     // 3.  Methods (make things happen)
+    func update(on canvas: Canvas) {
+    
+        // Move the circles
+        y += dy
+        x += dx
+        
+        // set circle colours
+        canvas.drawShapesWithFill = false
+        canvas.fillColor = Color.white
+        
+            // Draw the circle
+        canvas.drawEllipse(at: Point(x: x, y: y),
+                           width: diameter,
+                           height: diameter)
+        
+        // Bounce first cirlcle at edges
+        
+        if x >= canvas.width {
+
+            dx = -1
+
+        } else if x <= 0 {
+
+            dx = Int.random(in: -5...50)
+
+        }  else if y >= canvas.height {
+
+                dy = -1
+
+        }   else if y <= 0 {
+
+            dy = +1
+
+        }
+
+        
+    }
+    
 }
 
